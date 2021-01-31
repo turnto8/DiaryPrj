8a9cabc0681ed1869376a0b6bd7ff8690ef2a8be
JunyoungLee <turnto@daum.net>
2021-01-31 16:45:12 +0900

    test

---
+++ DiaryPrj/src/HelloWorld2/TestHelloWorld2.java
@@ -0,0 +1,14 @@
+package HelloWorld2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestHelloWorld2 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
